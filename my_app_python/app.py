c372ab149691f57e1fcbb51366fd9d1f4cca906b
lilac-zhang <lilac202123@gmail.com>
2026-04-27 18:03:23 +0900

    update app

--- my_app_python/app.py
+++ my_app_python/app.py
@@ -1,3 +1,9 @@
+import boto3
+import uuid
+
+s3 = boto3.client("s3")
+BUCKET = "zzw-myapp-images-123456"
+
 import os
 from dotenv import load_dotenv
 
@@ -37,7 +43,7 @@ init_db()
 @app.route("/")
 def index():
     cur = conn.cursor()
-    cur.execute("SELECT id, title, done FROM todos ORDER BY id DESC")
+    cur.execute("SELECT id, title, done, image_url FROM todos")
     todos = cur.fetchall()
     cur.close()
     return render_template("index.html", todos=todos)
@@ -45,17 +51,20 @@ def index():
 # add
 @app.route("/add", methods=["GET", "POST"])
 def add():
-    if request.method == "POST":
-        title = request.form["title"]
+    title = request.form["title"]
+    file = request.files.get("image")
 
-        cur = conn.cursor()
-        cur.execute("INSERT INTO todos (title, done) VALUES (%s, false)", (title,))
-        conn.commit()
-        cur.close()
+    image_url = None
 
-        return redirect("/")
+    if file:
+        filename = str(uuid.uuid4()) + "_" + file.filename
+        s3.upload_fileobj(file, BUCKET, filename)
+        image_url = f"https://{BUCKET}.s3.amazonaws.com/{filename}"
 
-    return render_template("add.html")
+    cur.execute(
+         "INSERT INTO todos (title, done, image_url) VALUES (%s, false, %s)",
+         (title, image_url)
+    )
 
 # edit
 @app.route("/<int:id>/edit", methods=["GET", "POST"])
